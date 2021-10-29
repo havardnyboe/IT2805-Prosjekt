3395dcba10a5c707ecd180d6f6e6052716a8c0b4
havardnyboe <havard.nyboe@gmail.com>
2021-10-29 12:56:20 +0200

    fixxa footer

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,6 +1,6 @@
 let footerHTML = `
-    <div id="telefon"><b>Tlf:</b><a href="tel:41322599">41322599</a></div>
-    <div id="email"><b>E-post:</b><a href="mailto:irene-bo@outlook.com">irene-bo@outlook.com</a></div>
+    <div id="telefon"><b>Tlf: </b><a href="tel:41322599">41322599</a></div>
+    <div id="email"><b>E-post: </b><a href="mailto:irene-bo@outlook.com">irene-bo@outlook.com</a></div>
     <div id="adresse"><b>Adresse: </b>Bettumveien 84, 3178 Våle</div>
 `;
 
