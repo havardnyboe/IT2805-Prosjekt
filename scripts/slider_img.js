75d6d72e6c8d0c79f7ec1b3f0a3d40dea30e4ec9
Mikkel Emil Lange Friis <mefriis@stud.ntnu.no>
2021-11-01 18:22:01 +0100

    Trolig fikset at timeSlide løper løpsk

--- scripts/slider_img.js
+++ scripts/slider_img.js
@@ -1,6 +1,11 @@
 
     // setter slide_index som 0 fordi den endres med en gang timeSlide begynner 
-let slide_index = 0
+// let slide_index = 0
+
+    // ligger inne noe kommentert-ut kode fordi jeg sliter med at timer løper løpsk av og til.. 
+            // prøver å finne det ut, men tror den fungerer som den skal nå
+    //edit som jeg tror beholdes, setter til 1 fordi timeSlide funksjonen er gjort om
+slide_index = 1
 
 // function currentSlide(i) {  
 //     displaySlides(slide_index = i);  
@@ -17,6 +22,9 @@ function displaySlides(n) {
 
     slides[slide_index - 1].style.display = "block";  
 }
+
+            // insp fra:
+            // https://www.c-sharpcorner.com/article/creating-an-image-slider-using-javascript-html-and-css-only/
 
 displaySlides(slide_index)
 
@@ -41,11 +49,19 @@ function keySlide(keypress){
 
     // Lagt til en timer som endrer bilde-slides
 let timeHandle = null
+console.log("tutut timer om 5s")
 
 function timeSlide (){
-    nextSlide(1)
-    timeHandle = setTimeout(timeSlide, 5000)
-    console.log("biip.. timer changing slide every 5s")
+    timeHandle = setTimeout(function(){
+        clearTimeout(timeHandle)
+        nextSlide(1)
+        console.log("biip.. timer changing slide every 5s")
+        timeSlide()
+    }, 5000)
+    // clearTimeout(timeHandle)
+    // nextSlide(1)
+    // timeHandle = setTimeout(timeSlide, 5000)
+    // console.log("biip.. timer changing slide every 5s")
 }
 
 timeSlide()
@@ -53,21 +69,18 @@ timeSlide()
     // delayTimer forsinket timeren, og kjører den igang igjen etter 30 sek
 function delayTimer () {
     clearTimeout(timeHandle)
-    console.log("Timer delayed by 30s")
+    console.log("Manuell endring. Timer forsinket m/ 30s")
     setTimeout(timeSlide, 30000)
 }
     // gjør at bilde-slider stopper når man forlater siden og begynner igjen når man åpner siden
 document.addEventListener("visibilitychange", function() {
-    if(document.visibilityState=="hidden"){
+    if(document.visibilityState == "hidden"){
         clearTimeout(timeHandle)
         console.log("Page hidden. Clear timeout for timeHandle")
     }
-    if(document.visibilityState=="visible"){
+    if(document.visibilityState == "visible"){
         // timeSlide()
         timeHandle = setTimeout(timeSlide, 5000)
         console.log("Page visible. Begin timeout 5s")
     }
 })
-
-    // insp fra:
-    // https://www.c-sharpcorner.com/article/creating-an-image-slider-using-javascript-html-and-css-only/
